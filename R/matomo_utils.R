ecbb3966bb7f4ff9ca63f6de38b30760c91b054f
MalditoBarbudo <victorgrandagarcia@gmail.com>
2025-04-24 12:46:51 +0200

    matomo_utils: matomo_get_latest added

--- R/matomo_utils.R
+++ R/matomo_utils.R
@@ -4,11 +4,11 @@
 #'
 #' This function assumes some environmental variables are available
 #'
-#' @param date_start Oldest date to retrieve
-#' @param date_end Newest date to retrieve
+#' @param last number of last records to retrieve
+#' @param period period to retrieve (day, week, month...)
 #'
 #' @export
-matomo_get_apps_events <- function(date_start = Sys.Date() - 31, date_end = Sys.Date()) {
+matomo_get_latest <- function(last = 12, period = "week") {
   # env vars needed
   matomo_url <- Sys.getenv("MATOMO_URL")
   matomo_token <- Sys.getenv("MATOMO_TOKEN")
@@ -17,9 +17,18 @@ matomo_get_apps_events <- function(date_start = Sys.Date() - 31, date_end = Sys.
   # site id's are not env vars because they are fixed values that we know
   matomo_sites <- 3:9
 
-  # query
-  glue::glue(
-    ""
-  )
+  # queries
+  matomo_queries <- glue::glue(
+    "{matomo_url}?module=API&method=Events.getAction&secondaryDimension=eventName&flat=1&idSite={matomo_sites}&date=last{last}&period={period}&format=json&token_auth={matomo_token}&filter_limit=-1"
+  ) |>
+    as.character() |>
+    purrr::set_names(c(
+      "lidar", "nfi", "allometr", "catdrought", "meteoland", "fes", "deboscat"
+    ))
+
+  json_responses <- matomo_queries |>
+    purrr::map(
+      .f = \(query) {jsonlite::read_json(query, simplifyVector = FALSE)}
+    )
 
 }
